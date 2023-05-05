90afe5f738b3b5d9fcb1243c7fdf2c4e1d3209ab
GOYEJIN <yaejingo@gmail.com>
2023-05-05 20:21:08 +0900

    Feat : UserService 기능 추가
    - 예외 처리 부분 수정 필요

---
+++ src/main/java/com/example/miniproject/service/UserService.java
@@ -0,0 +1,35 @@
+package com.example.miniproject.service;
+
+import com.example.miniproject.dto.LoginRequestDto;
+import com.example.miniproject.entity.User;
+import com.example.miniproject.config.jwt.JwtUtil;
+import com.example.miniproject.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.servlet.http.HttpServletResponse;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final JwtUtil jwtUtil;
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Transactional
+    public void login(LoginRequestDto loginRequestDto, HttpServletResponse response) {
+        String userId = loginRequestDto.getUserId();
+        String password = loginRequestDto.getPassword();
+
+        User user = userRepository.findByUserId(userId).orElseThrow(
+                () -> new IllegalArgumentException("dd"));// 예외처리 해주기
+
+        if (!passwordEncoder.matches(password, user.getPassword())) {
+            throw new IllegalStateException("dd");
+        }
+        response.addHeader(JwtUtil.AUTHORIZATION_HEADER, jwtUtil.createToken(user.getUserId()));
+    }
+}
