99fed77a4b42de1498ca386ff9caf62d9170dbde
Kim Seonmin <w.hand715@gmail.com>
2023-05-06 13:39:23 +0900

    Fix : aws 관련 인증 정보 분리
    application-aws.properties 에서 별도로 관리
    어플리케이션 실행 시 해당 properties 도 반영할 수 있도록 @PropertySource 추가

--- src/main/java/com/example/miniproject/MiniProjectApplication.java
+++ src/main/java/com/example/miniproject/MiniProjectApplication.java
@@ -2,10 +2,13 @@ package com.example.miniproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@PropertySource(value = {"classpath:application-aws.properties"})
 public class MiniProjectApplication {
 
 	public static void main(String[] args) {
