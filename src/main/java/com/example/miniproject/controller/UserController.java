db55810a625a05057719afb88237be33f191d2ae
GOYEJIN <yaejingo@gmail.com>
2023-05-08 01:55:02 +0900

    Feat : UserController 에 로그아웃 기능 추가

--- src/main/java/com/example/miniproject/controller/UserController.java
+++ src/main/java/com/example/miniproject/controller/UserController.java
@@ -6,10 +6,12 @@ import com.example.miniproject.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @RestController
@@ -20,8 +22,14 @@ public class UserController {
 
     @PostMapping("/user/login")
     public ResponseEntity<MsgAndHttpStatusDto> login(@RequestBody LoginRequestDto loginRequestDto, HttpServletResponse response) {
-        userService.login(loginRequestDto,response);
+        userService.login(loginRequestDto, response);
         return ResponseEntity.ok(new MsgAndHttpStatusDto("로그인 성공", HttpStatus.OK.value()));
 
+    }
+
+    @GetMapping("/user/logout")
+    public ResponseEntity<MsgAndHttpStatusDto> logout(HttpServletRequest request) {
+        userService.logout(request);
+        return ResponseEntity.ok(new MsgAndHttpStatusDto("로그아웃 성공", HttpStatus.OK.value()));
     }
 }
