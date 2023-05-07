abc45691e94f2f3c09d7f7a5b216a77d89ef5efa
GOYEJIN <yaejingo@gmail.com>
2023-05-08 02:03:30 +0900

    Fix : TokenRepository 삭제

--- src/main/java/com/example/miniproject/repository/TokenRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.miniproject.repository;
-
-import com.example.miniproject.entity.RefreshToken;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TokenRepository extends JpaRepository<RefreshToken, Long> {
-    boolean existsByRefreshToken(String refreshToken);
-}
--- src/main/java/com/example/miniproject/service/UserService.java
+++ src/main/java/com/example/miniproject/service/UserService.java
@@ -1,11 +1,8 @@
 package com.example.miniproject.service;
 
-import com.example.miniproject.config.jwt.JwtAuthenticationFilter;
 import com.example.miniproject.config.jwt.JwtUtil;
 import com.example.miniproject.dto.LoginRequestDto;
-import com.example.miniproject.entity.RefreshToken;
 import com.example.miniproject.entity.User;
-import com.example.miniproject.repository.TokenRepository;
 import com.example.miniproject.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -23,7 +20,6 @@ public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
-    private final TokenRepository tokenRepository;
 
     private final RedisService redisService;
 
@@ -48,7 +44,6 @@ public class UserService {
         String refreshToken = jwtUtil.createRefreshToken(user.getUserId());
 
         redisService.setValues(refreshToken, user.getUserId());
-        tokenRepository.save(new RefreshToken(refreshToken));
 
         response.addHeader(JwtUtil.AUTHORIZATION_HEADER, accessToken);
         response.addHeader(JwtUtil.REFRESHTOKEN_HEADER, refreshToken);
